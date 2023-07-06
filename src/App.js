d740cc3e87d1bf14ec328bd5c2c58a490cd43c7c
bhargavasai227 <112853876+bhargavasai227@users.noreply.github.com>
2023-07-06 16:40:56 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+ const [prompt,setP]=useState("a couple holding hands")
+ const delay = ms => new Promise(
+  resolve => setTimeout(resolve, ms)
+);
+  async function submit(e){
+    e.preventDefault()
+    setP("?")
+    await  delay(200)
+    alert("please wait for the image to be generated")
+    setP(e.target[0].value)
+    console.log(prompt);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <form onSubmit={submit}>
+      <textarea placeholder="a couple holding hands" />
+      <button type="submit" >submit</button>
+      </form>
+      <span>The default prompt is <i>a couple holding hands</i></span>
+      <img itemID='picture' alt="wait an instance!!" src ={"https://image.pollinations.ai/prompt/"+prompt}/>
+      <span>Wait for the prompt to be processed and returned!! </span>
     </div>
   );
 }
